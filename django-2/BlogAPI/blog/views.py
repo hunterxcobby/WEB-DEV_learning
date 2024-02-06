e8805db140a0c2f799487a97f98972a50a8e1ef4
hunterxcobby <solomonsefah13@gmail.com>
2024-02-06 18:43:16 +0000

    update blog app

--- django-2/BlogAPI/blog/views.py
+++ django-2/BlogAPI/blog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 # Create your views here.
+
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+class BlogPostList(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+class BlogPostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
 def blog_page(request):
     return render(request, "blog/index.html")
