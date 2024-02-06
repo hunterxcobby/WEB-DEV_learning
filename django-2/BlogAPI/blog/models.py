a2c34ab319205353df9d6421f7cd3c7070f4b039
hunterxcobby <solomonsefah13@gmail.com>
2024-02-06 18:43:05 +0000

    update blog app

--- django-2/BlogAPI/blog/models.py
+++ django-2/BlogAPI/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# model for blog post
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
