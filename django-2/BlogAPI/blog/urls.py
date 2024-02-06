dbace9a2e4d2485e823896fa1b17ae27c9378482
hunterxcobby <solomonsefah13@gmail.com>
2024-02-06 18:43:10 +0000

    update blog app

--- django-2/BlogAPI/blog/urls.py
+++ django-2/BlogAPI/blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_page, name="blog_page"),
+    path('blog-posts/', views.BlogPostList.as_view(), name='blogpost-list'),
+    path('blog-posts/<int:pk>/', views.BlogPostDetail.as_view(), name='blogpost-detail')
 ]
