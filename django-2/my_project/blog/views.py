e1270296eda61d5febc41889dbf6c465c8f79fde
hunterxcobby <solomonsefah13@gmail.com>
2024-02-23 23:00:55 +0000

    add results

--- django-2/my_project/blog/views.py
+++ django-2/my_project/blog/views.py
@@ -6,8 +6,8 @@ def blog(request):
     return render(request, 'blog.html', {'name': 'Cobby'} )
 
 def add(request):
-    val1 = request.GET['num1']
-    val2 = request.GET['num2']
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
     result = val1 + val2
 
     return render(request, 'result.html', {'result': result})
