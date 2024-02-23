cd5b0e226b68bd527b664a31a228c40824b07acb
hunterxcobby <solomonsefah13@gmail.com>
2024-02-23 22:22:15 +0000

    add static pages ;

--- django-2/my_project/my_project/settings.py
+++ django-2/my_project/my_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_)c-8lc_=e3l^114cvn@!59x#352(!o8o%#zah7i4ro^$@l^8r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
