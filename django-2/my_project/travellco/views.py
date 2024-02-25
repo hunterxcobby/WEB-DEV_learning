d0536912e84910cc033cb50d0778ebbd751e4284
hunterxcobby <solomonsefah13@gmail.com>
2024-02-25 19:01:52 +0000

    add respective images

--- django-2/my_project/travellco/views.py
+++ django-2/my_project/travellco/views.py
@@ -7,19 +7,19 @@ def index(request):
 
     dest1 = Destination()
     dest1.name = 'Koforidua'
-    dest1.img = 'destination_1.jpg'
+    dest1.img = 'koforidua.jpg'
     dest1.desc = 'Your comfort zone'
     dest1.price = 700
 
     dest2 = Destination()
     dest2.name = 'Accra'
-    dest2.img = 'destination_2.jpg'
+    dest2.img = 'accra.jpg'
     dest2.desc = 'The capital city for a reason'
     dest2.price = 1000
 
     dest3 = Destination()
     dest3.name = 'Kumasi'
-    dest3.img = 'destination_3.jpg'
+    dest3.img = 'kumasi.jpg'
     dest3.desc = 'The Garden City'
     dest3.price = 850
     
