1eec75f64627254099e3b52ae2cde0237aadf602
hunterxcobby <solomonsefah13@gmail.com>
2024-02-26 14:29:46 +0000

    make migrations

--- django-2/my_project/travellco/models.py
+++ django-2/my_project/travellco/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Destination(models.Model):
-    name = models.CharField(max_lenth=100)
+    name = models.CharField(max_length=100)
     img = models.ImageField(upload_to='pics')
     desc = models.TextField()
     price = models.IntegerField()
