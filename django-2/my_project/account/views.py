189796b021f2d36c548254e7e9740a23524e16bf
hunterxcobby <solomonsefah13@gmail.com>
2024-02-26 18:00:18 +0000

    add view

--- django-2/my_project/account/views.py
+++ django-2/my_project/account/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User, auth
 # Create your views here.
 
 def register(request):
-    
-    return render(request, 'register.html')
+
+    if request.method == 'POST':
+        first_name = request.POST['first-name']
+        last_name = request.POST['last-name']
+        user_name = request.POST['username']
+        password1 = request.POST['password']
+        password2 = request.POST['confirm-password']
+        email = request.POST['email']
+        
+        user = User.objects.create_user(username=username)
+
+    else:
+        return render(request, 'register.html')
