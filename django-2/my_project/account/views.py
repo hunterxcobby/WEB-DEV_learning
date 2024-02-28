305da43a48ca3fe1bed099208f5c517781c7b598
hunterxcobby <solomonsefah13@gmail.com>
2024-02-28 21:09:23 +0000

    authenticate the user

--- django-2/my_project/account/views.py
+++ django-2/my_project/account/views.py
@@ -17,9 +17,10 @@ def register(request):
         if password1 == password2:
             if User.objects.filter(user_name=user_name).exists():
                 print("Username taken already")
-            user = User.objects.create_user(username=user_name, password=password1, email=email, first_name=first_name, last_name=last_name)
-            user.save()
-            print("User Created")
+            else:
+                user = User.objects.create_user(username=user_name, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print("User Created")
         else:
             print("Password does not match, please confirm")
         return redirect('/')
