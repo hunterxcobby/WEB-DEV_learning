c3ff7c90747fa89d67a7de29d3737a47b153373a
hunterxcobby <solomonsefah13@gmail.com>
2024-02-29 12:49:41 +0000

    change database to postgresql

--- django-2/my_project/account/views.py
+++ django-2/my_project/account/views.py
@@ -15,7 +15,7 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            return redirect("/")
         else:
             messages.info(request, "invalid credentials")
             return redirect('login')
