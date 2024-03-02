2171ff680fbc12cacd45f831209eb100550fcad0
hunterxcobby <solomonsefah13@gmail.com>
2024-03-02 11:25:59 +0000

    fix assertion error

--- django-rest/myproject/api/views.py
+++ django-rest/myproject/api/views.py
@@ -1,10 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view('GET')
+@api_view(['GET'])
 def getData(request):
-    person = {
-        'name':'Dennis',
-        'age': 21,
-    }
+    person = {'name':'Dennis', 'age': 21}
     return Response(person)
