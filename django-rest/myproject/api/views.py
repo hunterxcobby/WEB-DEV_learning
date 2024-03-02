2cf3d9116ece93e8a25c682e8fd0ba0eeea621ac
hunterxcobby <solomonsefah13@gmail.com>
2024-03-02 11:13:46 +0000

    add data to views

--- django-rest/myproject/api/views.py
+++ django-rest/myproject/api/views.py
@@ -1,5 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view
+@api_view('GET')
 def getData(request):
+    person = {
+        'name':'Dennis',
+        'age': 21,
+    }
+    return Response(person)
