39f1fcc36857ba09133d23d02f85a1ac2d5fdaaa
hunterxcobby <solomonsefah13@gmail.com>
2024-03-25 11:50:23 +0000

    Fix typo in include statement

--- django-rest/blogApi/blogApi/urls.py
+++ django-rest/blogApi/blogApi/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogApiApp.urls')),
+    path('', include('blogAPiApp.urls')),
 ]
