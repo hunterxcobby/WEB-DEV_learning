bae96bdb208a3017817606287b7397cf14614fff
hunterxcobby <solomonsefah13@gmail.com>
2024-03-25 11:50:34 +0000

    Update URLs in blogAPiApp

--- django-rest/blogApi/blogAPiApp/urls.py
+++ django-rest/blogApi/blogAPiApp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/posts/', views.PostList.as_view(), name='post-list'),
-    path('api/posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+    path('', views.index, name='index'),
 ]
