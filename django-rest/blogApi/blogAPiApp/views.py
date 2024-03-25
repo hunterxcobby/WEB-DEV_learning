e77a9706f68ce5e7da900e4041dddd0f34416208
hunterxcobby <solomonsefah13@gmail.com>
2024-03-25 12:42:25 +0000

    Add create_post view to views.py

--- django-rest/blogApi/blogAPiApp/views.py
+++ django-rest/blogApi/blogAPiApp/views.py
@@ -9,9 +9,20 @@ from .serializers import postSerializer
 def index(request):
     return Response({"The setup was successful!"})
 
+
 @api_view(['GET'])
 def get_posts(request):
     get_all_posts = Post.objects.all()
     serializer = postSerializer(get_all_posts, many=True)
-    
+
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def create_post(request):
+    serializer = postSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
     return Response(serializer.data)
